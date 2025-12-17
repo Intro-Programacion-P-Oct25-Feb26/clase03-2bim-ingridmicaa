3a43c8661a58e20e4bd8ce297343fe01d01ff711
ingridmicaa <ingridmicaela03@gmail.com>
2025-12-17 12:55:08 -0500

    Ejercicioo

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -16,17 +16,42 @@ public class Ejercicio {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
-         
-        String[] estaciones = new String[5]; 
-        double [][] produccion = new double[5][12];
-        double [] total_produccion = new double[5];
-        
-        for(int f=0; f< estaciones.length; f++ ){
+
+        String[] estaciones = new String[5];
+        double[][] produccion = new double[5][13];
+        double[] totalProduccion = new double[5];
+
+        for (int f = 0; f < estaciones.length; f++) {
             System.out.println("Ingrese el nombre del encargado de la estación");
             estaciones[f] = entrada.nextLine();
-            
-            
-        }
 
+        }
+        for (int i = 0; i < produccion.length; i++) {
+
+            System.out.printf("Ingrese el ingreso mensual de: %s\n", estaciones);
+            for (int j = 1; j < produccion[i].length; j++) {
+
+                System.out.printf("Mes %s: $", j);
+
+                produccion[i][j] = entrada.nextDouble();
+                totalProduccion[i] = totalProduccion[i] + produccion[i][j];
+            }
+            entrada.nextLine();
+
+        }
+        int indice = 0;
+        for (int i = 1; i < 5; i++) {
+            if (totalProduccion[i] > totalProduccion[indice]) {
+                indice = i;
+            }
+        }
+        System.out.printf("Análisis de Producción\nEstación\n");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf(
+                    "Nombre Estación ", i
+                    + " - Total Producción: $ " + totalProduccion[i]);
+        }
     }
+
 }
